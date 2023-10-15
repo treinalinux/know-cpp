f69c1ccb39e2dc88a5e87d30541d2c59fbfd65a2
Alan da Silva Alves <linuxfama@gmail.com>
2023-10-15 17:50:35 -0300

    app was compiled for Linux 64

--- helloParOuImpar.cpp
+++ helloParOuImpar.cpp
@@ -5,6 +5,13 @@
 using namespace std;
 
 
+/*
+ * Compilado para Linux
+ *
+ * helloParOuImpar: ELF 64-bit LSB pie executable, x86-64, version 1 (SYSV), dynamically linked, interpreter /lib64/ld-linux-x86-64.so.2, BuildID[sha1]=976b718e5e90b2b68ee79555fe8dae45085c3f5e, for GNU/Linux 3.2.0, not stripped
+
+*/
+
 // Espera dois segundos ;)
 void doisSegundos(){
     std::this_thread::sleep_for( std::chrono::seconds(2) );
